a6ecec1db944a2568a0c4d29677cae0aa39fe83a
RAEES KHAN <inforaees6908090@gmail.com>
2025-12-18 22:52:40 +0500

    one change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ecommerce-frontend/", // <-- set this to your repo name
+  base: "/Ecommerce-website/", // <-- set this to your repo name
 });
